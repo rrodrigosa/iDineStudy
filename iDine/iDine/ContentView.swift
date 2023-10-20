514bdb15158828f453e36414819488d03cac6d5c
Rodrigo SA <rrodrigo.sa@gmail.com>
2023-10-20 18:35:34 -0300

    Add navigation stack and static menu list

--- iDine/iDine/ContentView.swift
+++ iDine/iDine/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                List {
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
+                .navigationTitle("Menu")
+            }
         }
-        .padding()
     }
 }
 
