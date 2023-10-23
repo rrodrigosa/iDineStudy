7cad4a85cc84eccf34996c3196407dcf3b0e57ec
Rodrigo SA <rrodrigo.sa@gmail.com>
2023-10-23 18:20:06 -0300

    Update restrictions information text style

--- iDine/iDine/ItemRow.swift
+++ iDine/iDine/ItemRow.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct ItemRow: View {
     let item: MenuItem
+    let restrictionsColors: [String: Color] = [
+        "D": .purple,
+        "G": .black,
+        "N": .red,
+        "S": .blue,
+        "V": .green]
     
     var body: some View {
         HStack {
@@ -22,8 +28,16 @@ struct ItemRow: View {
                 Text("$\(item.price)")
             }
             
+            Spacer()
+            
             ForEach(item.restrictions, id: \.self) { restriction in
                 Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(restrictionsColors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
