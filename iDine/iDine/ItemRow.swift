c404123cc2c14559d1d597c80120e8748958a2e3
Rodrigo SA <rrodrigo.sa@gmail.com>
2023-10-23 17:36:01 -0300

    Update thumbnail image with clipShape and overlay

--- iDine/iDine/ItemRow.swift
+++ iDine/iDine/ItemRow.swift
@@ -13,6 +13,8 @@ struct ItemRow: View {
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
             
             VStack(alignment: .leading) {
                 Text(item.name)
