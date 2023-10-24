9e6e173b216d7559d9b79b648ea00fd7fbb96be4
Rodrigo SA <rrodrigo.sa@gmail.com>
2023-10-24 16:23:16 -0300

    Add ItemDetail view to be used as a custom view
    The view contains the item details:
    - Image
    - Description

---
+++ iDine/iDine/ItemDetail.swift
@@ -0,0 +1,23 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Rodrigo SA on 24/10/23.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    let item: MenuItem
+    
+    var body: some View {
+        VStack {
+            Image(item.mainImage)
+            Text(item.description)
+        }.navigationTitle(item.name)
+    }
+}
+
+#Preview {
+    ItemDetail(item: MenuItem.example)
+}
