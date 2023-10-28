deacb92d687220ffd3fb65d95ddb569ea41168f1
Rodrigo SA <rrodrigo.sa@gmail.com>
2023-10-27 22:37:08 -0300

    feature: Add Form view, confirm order button and available order options
    Added order options for:
    - payment type
    - tip amount

--- iDine/iDine/Features/Checkout/Views/CheckoutView.swift
+++ iDine/iDine/Features/Checkout/Views/CheckoutView.swift
@@ -11,18 +11,36 @@ struct CheckoutView: View {
     @EnvironmentObject var order: Order
     
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
-    
+    let tipAmounts = [10, 15, 20, 25, 0]
+                      
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
     
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     
-                    ForEach(paymentTypes, id: \.self) {
-                        Text($0)
-                    }
+                    ForEach(paymentTypes, id: \.self) { Text($0) }
                 }
+                
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                
+                if addLoyaltyDetails {
+                    TextField("Add your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            Section("Add a tip?") {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) { Text("\($0)%") }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Total value: $100") {
+                Button("Confirm order") {}
             }
         }
         .navigationTitle("Payment")
